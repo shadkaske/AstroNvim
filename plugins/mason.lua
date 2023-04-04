1b02a5ea1b711f61289ece77eb156de950ed2d33
Shad Kaske <shadkaske@gmail.com>
2023-04-04 11:43:43 -0500

    Mason Config

--- plugins/mason.lua
+++ plugins/mason.lua
@@ -5,7 +5,23 @@ return {
     "williamboman/mason-lspconfig.nvim",
     -- overrides `require("mason-lspconfig").setup(...)`
     opts = {
-      -- ensure_installed = { "lua_ls" },
+      ensure_installed = {
+        "volar",
+        "ansiblels",
+        "bashls",
+        "cssls",
+        "tailwindcss",
+        "html",
+        "intelephense",
+        "jsonls",
+        "lua_ls",
+        "marksman",
+        "sqlls",
+        "emmet_ls",
+        "eslint",
+        "tsserver",
+        "yamlls",
+      },
     },
   },
   -- use mason-null-ls to configure Formatters/Linter installation for null-ls sources
@@ -13,14 +29,16 @@ return {
     "jay-babu/mason-null-ls.nvim",
     -- overrides `require("mason-null-ls").setup(...)`
     opts = {
-      -- ensure_installed = { "prettier", "stylua" },
+      ensure_installed = { "prettier", "stylua" },
     },
   },
   {
     "jay-babu/mason-nvim-dap.nvim",
     -- overrides `require("mason-nvim-dap").setup(...)`
     opts = {
-      -- ensure_installed = { "python" },
+      ensure_installed = {
+        "php-debug-adapter",
+      },
     },
   },
 }
