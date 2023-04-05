a03f7cc81dc63d430faa104461535a666bdb2fb1
Shad Kaske <shadkaske@gmail.com>
2023-04-05 11:12:19 -0500

    comments clean up

--- plugins/community.lua
+++ plugins/community.lua
@@ -1,16 +1,10 @@
 return {
-  -- Add the community repository of plugin specifications
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.colorscheme.catppuccin" },
   { import = "astrocommunity.debugging.nvim-bqf" },
   { import = "astrocommunity.motion.leap-nvim" },
   { import = "astrocommunity.motion.mini-move" },
   { import = "astrocommunity.motion.mini-surround" },
-  -- { import = "astrocommunity.utility.noice-nvim" },
   { import = "astrocommunity.editing-support.todo-comments-nvim" },
-  -- example of imporing a plugin, comment out to use it or add your own
-  -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
-
-  -- { import = "astrocommunity.colorscheme.catppuccin" },
-  -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.terminal-integration.flatten-nvim" },
 }
