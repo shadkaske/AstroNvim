5924015c15bb91be40fb2b1deb572ba200470e0f
Shad Kaske <shadkaske@gmail.com>
2023-04-05 07:46:40 -0500

    Added COmmunity plugins

--- plugins/community.lua
+++ plugins/community.lua
@@ -5,7 +5,9 @@ return {
   { import = "astrocommunity.debugging.nvim-bqf" },
   { import = "astrocommunity.motion.leap-nvim" },
   { import = "astrocommunity.motion.mini-move" },
-  { import = "astrocommunity.utility.noice-nvim" },
+  { import = "astrocommunity.motion.mini-surround" },
+  -- { import = "astrocommunity.utility.noice-nvim" },
+  { import = "astrocommunity.editing-support.todo-comments-nvim" },
   -- example of imporing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
